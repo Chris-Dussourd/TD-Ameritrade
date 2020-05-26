f269e402d0358f89334645706ec4881e57512257
Chris_Dussourd <crdussourd@gmail.com>
2020-05-26 14:45:45 -0700

    Add authentication python file to authenticate an app on TD Ameritrade.

---
+++ auth_info.py
@@ -0,0 +1,14 @@
+#Information needed to authenticate my TD developer account
+consumer_key='ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+redirect_uri='https://localhost/'
+client_id=consumer_key+'@AMER.OAUTHAP'
+
+#Information for logging into the TD site
+username = 'User1234'
+password = 'password_secure'
+
+#Save the refresh token in here
+refresh_token='29ip4uwkfsmgh45uw5'
+
+#TD Ameritrade Account number
+account_num=123456789
